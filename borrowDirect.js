f05c0d28d71d0f440f6e0b4a7f4cf00e10bb4ed7
Corinna Baksik <corinna_baksik@harvard.edu>
2015-02-10 14:35:22 -0500

    BD div changes

--- borrowDirect.js
+++ borrowDirect.js
@@ -16,14 +16,14 @@ function borrowDirect() {
 		var borrowDirectHTML = '<div class="borrowDirect">';
 		borrowDirectHTML += '<span class="borrowDirectHeader">Not available at Harvard ?</span>';
 
-		borrowDirectHTML += '<span class="borrowDirectText">See <a href="http://nrs.harvard.edu/urn-3:hul.eother:GetIt" target="_blank">Get It</a> options, or search ';
+		borrowDirectHTML += '<span class="borrowDirectText">';
 
 		if (firstISBN.length > 0)
 			borrowDirectHTML += '<a href="http://access.harvard.edu/access/servlet/access?__hulaccess_gateway=bdirect&__hulaccess_resource=&query=' + encodeURIComponent('isbn=' + firstISBN) + '" target="_blank">';
 		else
 			borrowDirectHTML += '<a href="http://access.harvard.edu/access/servlet/access?__hulaccess_gateway=bdirect&__hulaccess_resource=&query=' + encodeURIComponent(recordTitle) + '" target="_blank">';
 
-		borrowDirectHTML += 'Borrow Direct</a> now.</span>';
+		borrowDirectHTML += 'Request item</a> for 4-day delivery through Borrow Direct. Or, see <a href="http://nrs.harvard.edu/urn-3:hul.eother:GetIt" target="_blank">Get It</a> for other options.</span>';
 		borrowDirectHTML += '</div>';
 
 		$(this).find(".EXLLocationListContainer").append(borrowDirectHTML);
