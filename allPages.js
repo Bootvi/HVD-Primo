d03465a9c8291ecfcfbfabf14b421ad0207c2510
Corinna Baksik <corinna_baksik@harvard.edu>
2015-02-18 16:45:35 -0500

    re-adding icon to FA tab link

--- allPages.js
+++ allPages.js
@@ -130,7 +130,7 @@ function addEADTab() {
 			if (eadId) {
 				//Build a link and add it to the tab list
 				var findingAidLink = "http://oasis.lib.harvard.edu/oasis/primo?id=" + eadId + "&q=" + $("#search_field").val();;
-				var findingAidTab = '<li class="EXLFindingAids EXLResultTab"><a target="_blank" href="' + findingAidLink + '">Finding Aids</a></li>';
+				var findingAidTab = '<li class="EXLFindingAids EXLResultTab"><a target="_blank" href="' + findingAidLink + '">Finding Aids<img class="eadlinkicon" src="../images/icon_popout_tab.png" alt="Open in a new tab"></a></li>';
 				$(this).find(".EXLResultTabs").append(findingAidTab);
 
 			}
