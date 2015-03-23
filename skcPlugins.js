975a1084a9ff16b2bc78dce8e435bd84b31e0a6d
Corinna <cbaksik@gmail.com>
2015-03-23 16:02:47 -0400

    SCK tweaks

--- skcPlugins.js
+++ skcPlugins.js
@@ -16,7 +16,7 @@ function buildSKCgallery() {
 		}
 	
 		//Create Gallery header
-		var galleryHeaderHTML = '<div id="skcGalleryHeader">SKC TEST header: (Click on an image to enlarge)</div>';
+		var galleryHeaderHTML = '<div id="skcGalleryHeader">Select digitized content (click on an image to enlarge)</div>';
 		$(this).append(galleryHeaderHTML);
 
 		//Create a gallery area
@@ -36,67 +36,7 @@ function buildSKCgallery() {
 
 		document.getElementById("skcGallery").innerHTML = skcHTML;
 
-		
-		// THIS IS WHERE I STOPPED
 
-		// REMOVE?
-		//Append the converted VIA XML to the gallery, and set 'rel' parameter to make them 1 gallary 
-		/* $(this).find(".VIAGallary").append(transformXSL(viaXML, "../uploaded_files/HVD/viaThumbnail.xsl"));
-		$(this).find("a.fancybox").attr("rel", recordId); */
-
-		// REMOVE?
-		//Trim the cpations
-		/* $(this).find(".VIAThumbnailTitle").each(function() {
-			var tlength = $(this).text().length;
-			if (tlength > 44) {
-				$(this).attr("title", $(this).text());
-				var text = $(this).text().substr(0, 44);
-				$(this).text(text.substr(0, text.lastIndexOf(" ")) + "...");
-			}
-
-		}); */
-
-		// REMOVE?
-		//Make it FANCYBOX, with MetaData function call to populate the information
-		/* $(this).find(".VIAGallary a.fancybox").fancybox({
-			openEffect: 'none',
-			closeEffect: 'none',
-			nextEffect: 'none',
-			prevEffect: 'none',
-			width: '1200',
-			height: 660,
-			maxHeight: 700,
-			margin: [20, 60, 20, 60],
-			helpers: {
-				title: {
-					type: 'inside'
-				},
-				overlay: {
-					locked: false
-				}
-			},
-			afterLoad: function(current, previous) {
-				modifyContents(current, previous);
-			},
-			onUpdate: function() {
-				resizeFancyBox();
-
-			}
-		}); */
-	});
-}
-
-
-
-//Builds a header based on lds41
-/* function createHeader(element, keycontent) {
-	var gallaryHeaderHTML = '<span class="skcGalleryHeader">';
-
-	if (keycontent)
-		galleryHeaderHTML += 'KEY CONTENT';
-	element.append(galleryHeaderHTML);
-} 
- */
 
 //REMOVE? 
 //Resize the box if image is narrower than 800px
