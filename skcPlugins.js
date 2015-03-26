692b9700bd32d0c537b1ff26e9af731777a0a332
Corinna <cbaksik@gmail.com>
2015-03-26 15:15:46 -0400

    skc and hvd.css changes
    adding data to lds41 instead of mis2. reading html to create gallergy

--- skcPlugins.js
+++ skcPlugins.js
@@ -2,7 +2,7 @@ function buildSKCgallery() {
 	$(".EXLDetailsContent li[id^='lds41']").parents(".EXLDetailsContent").each(function() {
 		
 		//Avoid duplicate work
-		if ($(this).find("#SKCgallery").length)
+		if ($(this).find("#skcGalleryHeader").length)
 			return;
 		
 		//Make this details tab larger
@@ -18,23 +18,21 @@ function buildSKCgallery() {
 		//Create Gallery header
 		var galleryHeaderHTML = '<div id="skcGalleryHeader">Select digitized content (click on an image to enlarge)</div>';
 		$(this).append(galleryHeaderHTML);
-
-		//Create a gallery area
-		$(this).append('<div id="skcGallery">SKC content test</div>');
 		
-		//Get the SKC data from the PNX record
+		//Get the SKC data from lds41
 		var recordId = $(this).parents(".EXLResult").find(".EXLResultRecordId").attr("id");
-		var pnxXML = loadPNX(recordId);				
-		var skcXML = $(pnxXML).find("addata > mis2");
+		var skcArray = $("li[id^='lds41'] .EXLDetailsDisplayVal").map(function() { return $(this).text() }).get();
 		var skcHTML = "";
-		for	(index = 0; index < skcXML.length ; index++) {
-			//split elements into labels and URLs, append to make gallery with thumbnails
-			var entry = skcXML[index].textContent;
-			var split = entry.split(";",2);
+		//console.log(skcArray.length);
+		//split elements into labels and URLs, append to make gallery with thumbnails
+		for	(index = 0; index < skcArray.length ; index++) {			
+			var entry = skcArray[index];
+			var split = entry.split(": ",2);	
 			skcHTML += '<div class="skcItem"><a href="'+split[1]+'?buttons=y" target="_blank"><img src="'+split[1]+'?height=375" height="375px" /></a><br /><span class="skcLabel">'+split[0]+'</span></div>';
-		} 
-
-		document.getElementById("skcGallery").innerHTML = skcHTML;
+		}  
+		//Create a gallery area
+		$(this).append('<div id="skcGallery-'+recordId+'">SKC content test</div>');		
+		document.getElementById("skcGallery-"+recordId).innerHTML = skcHTML;
 
 	});
 }
