0b844ab14727bdd67001c7cdb4c49c5eed56b5ee
Corinna <cbaksik@gmail.com>
2015-06-09 17:04:51 -0400

    hvd.css requestTab.js edits

--- requestTab.js
+++ requestTab.js
@@ -1,10 +1,16 @@
 // display patron notes if item status is on order or ordered received
 function displayPatronNotesHoldRequest() {	
-	if ($("td:contains('Item status: On order')").length) {
+	if ($("td:contains('Item status: On order')").length || $("td:contains('Item status: Ordered--received')").length) {
 		$("label[for^='exlidRequestTabFormInputComment']").show()
-		console.log("on order");
+		$("input[id^='exlidRequestTabFormInputComment']").show()
+		//console.log("on order");
+		// make input type date for 2nd comment, must go to Aleph as YYYY-MM-DD for Cognos reporting purposes 
+		//$("input[id='exlidRequestTabFormInputComment2']").prop("type","date");   not supported
+		$("input[id='exlidRequestTabFormInputComment2']").prop("pattern","^\\d\\d\\d\\d-\\d\\d-\\d\\d");   
+		$("input[id='exlidRequestTabFormInputComment2']").prop("title","Please enter date in YYYY-MM-DD format, e.g. 2016-02-20");   
 	} else {
-		console.log("NOT on order");
+		//console.log("NOT on order");
+		return;
 	}
 }
 
