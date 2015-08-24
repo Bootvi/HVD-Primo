61b1f52f3132d8b05efbfa40a7be542be28d6947
Corinna <cbaksik@gmail.com>
2015-08-24 14:27:38 -0400

    leaflet API change

--- hglPlugins.js
+++ hglPlugins.js
@@ -86,13 +86,18 @@ function osmItegration() {
 
 		//Creating the map dynamic object
 		var map = L.map('hglMap' + recordId).setView([centerLatitude, centerLongitude], 9);
-		L.tileLayer('https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', {
+		//20150824 CB changing L.titleLayer ... due to change in Leaflet API, per Dave S code
+		/*L.tileLayer('https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', {
 			maxZoom: 18,
 			attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
 				'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
 				'Imagery &copy; <a href="http://mapbox.com">Mapbox</a>',
 			id: 'examples.map-i875mjb7'
+		}).addTo(map); */
+		L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', { 
+			attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors' 
 		}).addTo(map);
+
 
 		//Setting the Box bounderies
 		if (coordinates[0] == coordinates[1] && coordinates[2] == coordinates[3])
